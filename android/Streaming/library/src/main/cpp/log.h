22d1fb63ef0485eaf647ebcc2f41973e1e41130f
wlanjie <wlanjie888@gmail.com>
2017-06-30 15:33:57 +0800

    remove code

--- android/Streaming/library/src/main/cpp/log.h
+++ android/Streaming/library/src/main/cpp/log.h
@@ -2,15 +2,15 @@
 // Created by wlanjie on 16/4/26.
 //
 
-#ifndef FFMPEG_LOG_H
-#define FFMPEG_LOG_H
+#ifndef STREAMING_LOG_H
+#define STREAMING_LOG_H
 
 #include <android/log.h>
 
-#define TAG "ffmpeg"
+#define TAG "streaming"
 
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG,  __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG ,__VA_ARGS__)
 
-#endif //FFMPEG_LOG_H
+#endif //STREAMING_LOG_H
